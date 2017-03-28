d4a72b94e030b21e6c1e241ee941659f55717d88
ashwintk <tashwin@okstate.edu>
2017-03-28 12:31:01 -0500

    Update HBaseTemperatureImporter.java

--- HBaseExample/src/edu/okstate/cs/tashwin/Temperature/HBaseTemperatureImporter.java
+++ HBaseExample/src/edu/okstate/cs/tashwin/Temperature/HBaseTemperatureImporter.java
@@ -1,11 +1,11 @@
-package edu.okstate.cs.tashwin.Temperature;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -21,31 +21,30 @@ import org.apache.hadoop.util.ToolRunner;
  * 1. Allowing the users to specify the table name to import contents to*/
 
 public class HBaseTemperatureImporter extends Configured implements Tool {
-  
-  static class HBaseTemperatureMapper<K> extends Mapper<LongWritable, Text, K, Put> {
+	private static final String NAME = "Temperature Importer";
+	
+  static class HBaseTemperatureMapper<K> extends Mapper<LongWritable, Text, ImmutableBytesWritable, Put> {
     private NcdcRecordParser parser = new NcdcRecordParser();
 
-    @SuppressWarnings("deprecation")
-	@Override
     public void map(LongWritable key, Text value, Context context) throws
         IOException, InterruptedException {
       parser.parse(value.toString());
       if (parser.isValidTemperature()) {
-        byte[] rowKey = RowKeyConverter.makeObservationRowKey(parser.getStationId(),
-            parser.getObservationDate().getTime());
+        byte[] rowKey = RowKeyConverter.makeObservationRowKey(parser.getStationId(), parser.getObservationDate().getTime());
         Put p = new Put(rowKey);
-        p.add(HBaseTemperatureQuery.DATA_COLUMNFAMILY,
-            HBaseTemperatureQuery.AIRTEMP_QUALIFIER,
-            Bytes.toBytes(parser.getAirTemperature()));
-        context.write(null, p);
+        p.addColumn(HBaseTemperatureQuery.DATA_COLUMNFAMILY, HBaseTemperatureQuery.AIRTEMP_QUALIFIER, Bytes.toBytes(parser.getAirTemperature()));
+        context.write(new ImmutableBytesWritable(rowKey), p);
       }
     }
   }
+  
+  
+  
 
   @Override
   public int run(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("Usage: HBaseTemperatureImporter <input> <table-name>");
+    if (args.length < 2) {
+      System.out.println("Usage: HBaseTemperatureImporter <input> <table-name>");
       return -1;
     }
     String inputPath = args[0];
@@ -53,28 +52,29 @@ public class HBaseTemperatureImporter extends Configured implements Tool {
     
     /*Check whether input path is null/empty */
     if (inputPath.length() == 0 || inputPath == null){
-    	System.err.println("Input Path cannot be null/empty");
+    	System.out.println("Input Path cannot be null/empty");
         return -1;
-    }
+    } 
     /*Check whether HTable name is null/empty */
     if (hbaseTable.length() == 0 || hbaseTable == null){
-    	System.err.println("HTable name cannot be null/empty");
+    	System.out.println("HTable name cannot be null/empty");
         return -1;
     }
-    @SuppressWarnings("deprecation")
-	Job job = new Job(getConf(), getClass().getSimpleName());
+	Job job = Job.getInstance(getConf(), HBaseTemperatureImporter.getName());
     job.setJarByClass(getClass());
-    FileInputFormat.addInputPath(job, new Path(inputPath));
-    job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, hbaseTable);
     job.setMapperClass(HBaseTemperatureMapper.class);
+    TableMapReduceUtil.initTableReducerJob(hbaseTable, null, job);
     job.setNumReduceTasks(0);
-    job.setOutputFormatClass(TableOutputFormat.class);
+    FileInputFormat.addInputPath(job, new Path(inputPath));
     return job.waitForCompletion(true) ? 0 : 1;
   }
 
   public static void main(String[] args) throws Exception {
-    int exitCode = ToolRunner.run(HBaseConfiguration.create(),
-        new HBaseTemperatureImporter(), args);
+    int exitCode = ToolRunner.run(HBaseConfiguration.create(), new HBaseTemperatureImporter(), args);
     System.exit(exitCode);
   }
+
+public static String getName() {
+	return NAME;
+}
 }
